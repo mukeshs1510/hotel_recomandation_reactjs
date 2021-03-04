898fcf279a2ea159f046c7c6617aec7fd0fbeb0b
Mukesh Suthar <49620706+mukeshs1510@users.noreply.github.com>
2021-03-04 21:33:46 +0530

    Added ReactBootStrap and done with Navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import "./App.css";
+import * as ReactBootStrap from "react-bootstrap";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h2>Hello World</h2>
-      </header>
+      <ReactBootStrap.Navbar bg="light" expand="lg">
+        <ReactBootStrap.Navbar.Brand href="#home">
+          <h2>HRS</h2>
+        </ReactBootStrap.Navbar.Brand>
+        <ReactBootStrap.Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <ReactBootStrap.Navbar.Collapse id="basic-navbar-nav">
+          <ReactBootStrap.Nav className="ml-auto mr-50">
+            <ReactBootStrap.Nav.Link className="hover" href="#home">
+              Home
+            </ReactBootStrap.Nav.Link>
+            <ReactBootStrap.Nav.Link href="#link">
+              Services
+            </ReactBootStrap.Nav.Link>
+          </ReactBootStrap.Nav>
+        </ReactBootStrap.Navbar.Collapse>
+      </ReactBootStrap.Navbar>
     </div>
   );
 }
